f4f659c752f6d8e2a93df38f35d2db400d10c37a
Pratik Kamble <pratikworkadd@gmail.com>
2025-01-31 18:11:22 +0530

    1- post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -26,6 +27,7 @@ console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} `);

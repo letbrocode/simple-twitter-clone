8a91ba42c73cf37f69a63f8b6f1786aa3ca03621
Pratik Kamble <pratikworkadd@gmail.com>
2025-02-07 17:56:13 +0530

    3- useFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -39,7 +39,6 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
-      //TODO: return the id of the user as a response
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
@@ -52,7 +51,7 @@ export const followUnfollowUser = async (req, res) => {
         to: userToModify._id,
       });
       await newNotification.save();
-      //TODO: return the id of the user as a response
+
       res.status(200).json({ message: "User followed successfully" });
     }
   } catch (error) {
---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+const useFollow = () => {
+  const queryClient = useQueryClient();
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: async (userId) => {
+      try {
+        const res = await fetch(`/api/users/follow/${userId}`, {
+          method: "POST",
+        });
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return res.json();
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+      ]);
+    },
+    onError: () => {
+      toast.error(error.message);
+    },
+  });
+  return { follow, isPending };
+};
+
+export default useFollow;
